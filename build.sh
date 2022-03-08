507b143158e5537879be23416320fccb95680a7a
Chris Currie <chriscuz83@outlook.com>
2022-03-08 17:31:19 +0000

    added cython example in nisas format plus test

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 python compile.py build_ext --inplace
+mv nisas/core.py nisas/old_core.txt
+mv nisas/core.c nisas/old_core_c.txt
+
+pytest
+
+mv nisas/old_core.txt nisas/core.py
+mv nisas/old_core_c.txt nisas/core.c
